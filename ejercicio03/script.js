1015e3a3fbad531d515b2aecdcc7a1535dd7ec67
Vieyra <vieyraurenabravo@gmail.com>
2020-06-23 20:07:06 -0600

    Agrego el ejercicio03

---
+++ ejercicio03/script.js
@@ -0,0 +1,12 @@
+let lineas = '';
+let a = 0;
+
+while(a < 8){
+  lineas = lineas + '';
+  a = a + 1;
+  while (a < 8) {
+    lineas = lineas + '* ';
+    a = a + 1;
+    console.log(lineas)
+  } 
+}
