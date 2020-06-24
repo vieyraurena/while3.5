25086c4bb1395867debe22442d73629bee60dcd1
Vieyra <vieyraurenabravo@gmail.com>
2020-06-23 20:06:16 -0600

    Agrego el ejercicio01

---
+++ ejercicio01/script.js
@@ -0,0 +1,12 @@
+//que quiero que se repita? la linea
+//que valor quiero cambiar cada ves, el asterisco
+//quiero dejar de repetir hasta 8
+
+let lineas = '*';
+let a = 1;
+
+while (a < 8) {
+  lineas=  lineas + ' ';
+  a = a + 1;
+  console.log(lineas)
+}
